077d295c17695ec58a04470bd896bec4aeae153f
Chris Gottfredson <chrisgottfredson@gmail.com>
2020-05-16 22:31:18 -0600

    added Sunrise and Sunset to Current coredataModel

---
+++ WeatherTheStorm/WeatherTheStorm/Helpers/StringExtension.swift
@@ -0,0 +1,20 @@
+//
+//  StringExtension.swift
+//  WeatherTheStorm
+//
+//  Created by Chris Gottfredson on 5/16/20.
+//  Copyright © 2020 Gottfredson. All rights reserved.
+//
+
+import Foundation
+
+extension String {
+    
+    func stringToDate() -> Date {
+        let formatter = DateFormatter()
+        formatter.locale = .current
+        formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZ"
+        guard let date = formatter.date(from: self) else { return Date()}
+        return date
+    }
+}
